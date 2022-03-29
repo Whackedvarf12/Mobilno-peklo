7965e10e10ebebe15fc48706b56a4469fc143772
Aly <aly.elkabani@gmail.com>
2022-03-29 08:12:13 +0200

    Hub git

---
+++ Test/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey kids, want to git some hubs?");
+            Console.ReadLine();
+        }
+    }
+}
