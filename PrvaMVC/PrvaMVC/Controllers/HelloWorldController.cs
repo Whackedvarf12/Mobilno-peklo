bde2e44422cbe7f31826c53515abb7f20219feed
Aly <aly.elkabani@gmail.com>
2022-04-01 10:09:40 +0200

    splet

---
+++ PrvaMVC/PrvaMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrvaMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "Poz";
+        }
+        public string Poz(string ime)
+        {
+            return ime;
+        }
+    }
+}
